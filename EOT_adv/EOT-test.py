85caae5e8c0c12263dcf2a40165ca8c6f8583ba2
hcyray <chuangak@connect.ust.hk>
2018-09-14 22:50:44 +0800

    update

--- EOT_adv/EOT-test.py
+++ EOT_adv/EOT-test.py
@@ -22,7 +22,7 @@ from cleverhans.attacks import FastGradientMethod, SaliencyMapMethod
 from cleverhans.utils_tf import model_eval, model_argmax
 from cleverhans import utils
 from distutils.version import LooseVersion
-import time
+
 
 #from cleverhans.attacks import CarliniWagnerL2  
 
@@ -33,7 +33,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # parameters
-dataDir = './training_raw/'
+dataDir = '../training_raw/'
 FS = 300
 WINDOW_SIZE = 30*FS     # padding window for CNN
 classes = ['A', 'N', 'O','~']
@@ -44,7 +44,7 @@ sess = tf.Session()
 K.set_session(sess)
 
 print("Loading model")    
-model = load_model('ResNet_30s_34lay_16conv.hdf5')
+model = load_model('../ResNet_30s_34lay_16conv.hdf5')
 #model = load_model('weights-best_k0_r0.hdf5')
 
 wrap = KerasModelWrapper(model)
@@ -81,7 +81,7 @@ preds = model(x)
 id = 9
 count = id-1
 record = "A{:05d}".format(id)
-local_filename = "./training_raw/"+record
+local_filename = "../training_raw/"+record
 # Loading
 mat_data = scipy.io.loadmat(local_filename)
 print('Loading record {}'.format(record))    
@@ -97,23 +97,21 @@ print('Ground truth:{}'.format(ground_truth))
 X_test=np.float32(data)
 Y_test = np.zeros((1, 1))
 Y_test[0,0] = ground_truth
-Y_test = utils.to_categorical(Y_test, num_classes=4)
+Y_tes0t = utils.to_categorical(Y_test, num_classes=4)
 
 target_a = np.array([0, 1, 0, 0]).reshape(1,4)
 target_a = np.float32(target_a)
 
-## myattacks CWL2
-from myattacks import CarliniWagnerL2  
-cwl2 = CarliniWagnerL2(wrap, sess=sess)
-cwl2_params = {'y_target': target_a, 'max_iterations': 100}
+from EOT_adv.EOT import EOT_L2
+cwl2 = EOT_L2(wrap, sess=sess)
+cwl2_params = {'y_target': target_a}
 adv_x = cwl2.generate(x, **cwl2_params)
 adv_x = tf.stop_gradient(adv_x) # Consider the attack to be constant
 #preds_adv = model(adv_x)
 feed_dict = {x: X_test}
 #adv_sample = sess.run(adv_x, feed_dict=feed_dict)
-start_time = time.time()
 adv_sample = adv_x.eval(feed_dict=feed_dict, session = sess)
-print("--- %s seconds ---" % (time.time() - start_time))
+
 #adv_sample = cwl2.generate_np(X_test, **cwl2_params)
 
 prob = model.predict(adv_sample)
@@ -121,27 +119,26 @@ ann = np.argmax(prob)
 ann_label = classes[ann]
 print(ann)
 
-
-ymax = np.max(adv_sample)+0.5
-ymin = np.min(adv_sample)-0.5
-
-fig, axs = plt.subplots(2, 1, figsize=(50,40))
-
-axs[0].plot(X_test[0,0:4000,:])
-axs[0].set_title('Original signal {}'.format(ground_truth_label))
-axs[0].set_ylim([ymin, ymax])
-axs[0].set_xlabel('index')
-axs[0].set_ylabel('signal value')
-
-axs[1].plot(adv_sample[0,0:4000,:])
-axs[1].set_title('Adversarial signal {}'.format(ann_label))
-axs[1].set_ylim([ymin, ymax])
-axs[1].set_xlabel('index')
-axs[1].set_ylabel('signal value')
-'''
-axs[2].plot(adv_sample[0,:]-X_test[0,:])
-axs[2].set_title('perturbations')
-axs[2].set_ylim([ymin, ymax])
-axs[2].set_xlabel('index')
-axs[2].set_ylabel('signal value')
-'''
+#
+#ymax = np.max(adv_sample)+0.5
+#ymin = np.min(adv_sample)-0.5
+#
+#fig, axs = plt.subplots(1, 3, figsize=(20,5))
+#
+#axs[0].plot(X_test[0,:])
+#axs[0].set_title('Original signal {}'.format(ground_truth_label))
+#axs[0].set_ylim([ymin, ymax])
+#axs[0].set_xlabel('index')
+#axs[0].set_ylabel('signal value')
+#
+#axs[1].plot(adv_sample[0,:])
+#axs[1].set_title('Adversarial signal {}'.format(ann_label))
+#axs[1].set_ylim([ymin, ymax])
+#axs[1].set_xlabel('index')
+#axs[1].set_ylabel('signal value')
+#
+#axs[2].plot(adv_sample[0,:]-X_test[0,:])
+#axs[2].set_title('perturbations')
+#axs[2].set_ylim([ymin, ymax])
+#axs[2].set_xlabel('index')
+#axs[2].set_ylabel('signal value')
