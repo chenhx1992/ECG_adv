5a8fd88cf62a2a192a41c29a7333d6fa3e9fd01a
hcyray <chuangak@connect.ust.hk>
2018-10-20 02:28:14 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -2,13 +2,14 @@ import numpy as np
 import tensorflow as tf
 
 sess = tf.InteractiveSession()
+tf_dtype = tf.as_dtype('float32')
 
 # Some tensor we want to print the value of
-a = tf.constant([[11],[12],[11],[12],[13],[11],[12]])
+a = tf.constant([[11],[12],[11],[12],[13],[11],[12]],dtype=tf_dtype)
 
 b = tf.nn.softmax(a)
 
-c = tf.constant([11,12,11,12,13,11,12])
+c = tf.constant([11,12,11,12,13,11,12],dtype=tf_dtype)
 d = tf.nn.softmax(c)
 print(b.eval())
 print(d.eval())
