2297b5fed8256a62b612e836ffb31ef3625b5c26
hcyray <chuangak@connect.ust.hk>
2018-10-20 02:26:34 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,6 +1,14 @@
 import numpy as np
+import tensorflow as tf
 
+sess = tf.InteractiveSession()
 
-a = np.array([[1,2,3],[4,5,6]])
-for i in range(10,20):
-    print(i)
+# Some tensor we want to print the value of
+a = tf.constant([[11][12][11][12][13][11][12]])
+
+b = tf.nn.softmax(a)
+
+c = tf.constant([11,12,11,12,13,11,12])
+d = tf.nn.softmax(c)
+print(b.eval())
+print(d.eval())
