3207a972f979d280f96e7747b4de3af363e010a3
hcyray <chuangak@connect.ust.hk>
2018-10-23 16:19:51 +0800

    update

--- EOT_adv/UniversalPerturbEval.py
+++ EOT_adv/UniversalPerturbEval.py
@@ -65,9 +65,9 @@ attack_success_all = np.zeros((4),dtype=int)
 
 #loading perturbation
 perturb_window = 200
-ensemble_size = 200
+ensemble_size = 50
 id_perturb = 65
-target = 0
+target = 3
 
 inputstr = '../output/EOTtile_w'+str(perturb_window)+'_e'+str(ensemble_size)+'_l2_A'+str(id_perturb)+'T'+str(target)+'.out'
 print("input file: ", inputstr)
