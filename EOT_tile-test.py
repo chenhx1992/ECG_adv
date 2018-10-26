408a242bb24c7e098c0f9141ec689a5bf22c17f9
hcyray <chuangak@connect.ust.hk>
2018-10-26 20:53:13 +0800

    update

--- EOT_tile-test.py
+++ EOT_tile-test.py
@@ -103,7 +103,7 @@ start_time = time.time()
 perturb_window = int(sys.argv[4])
 ensemble_size = int(sys.argv[5])
 eotl2 = EOT_ATTACK(wrap, sess=sess)
-eotl2_params = {'y_target': target_a, 'learning_rate': 0.1, 'max_iterations': 500, 'initial_const': 1000, 'perturb_window': perturb_window, 'dis_metric': dis_metric, 'ensemble_size': ensemble_size, 'ground_truth': ground_truth_a}
+eotl2_params = {'y_target': target_a, 'learning_rate': 0.5, 'max_iterations': 500, 'initial_const': 1000, 'perturb_window': perturb_window, 'dis_metric': dis_metric, 'ensemble_size': ensemble_size, 'ground_truth': ground_truth_a}
 
 adv_x = eotl2.generate(x, **eotl2_params)
 adv_x = tf.stop_gradient(adv_x) # Consider the attack to be constant
