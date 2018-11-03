49cdcc2f63c57b76ad7fe56f041f349ef31d9c70
hcyray <chuangak@connect.ust.hk>
2018-11-03 10:39:45 +0800

    Update EOT_tile-largetest.py

--- EOT_tile-largetest.py
+++ EOT_tile-largetest.py
@@ -85,7 +85,7 @@ target_len = target_file[:,2]
 ## Loading time serie signals
 k = 0
 ooo = np.zeros(6)
-while k<7:
+while k<1:
     
     start_time = time.time()
     ind = random.randint(0,len(target_id)-1)
