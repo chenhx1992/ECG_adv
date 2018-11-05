c010caa4ae1642b857cc1db2d211f11bf448ef29
hcyray <chuangak@connect.ust.hk>
2018-11-05 20:26:04 +0800

    Update EOT_tile_tf.py

--- EOT_tile_tf.py
+++ EOT_tile_tf.py
@@ -356,7 +356,7 @@ class EOT_tf_ATTACK(object):
                         bestl2[e] = xe
                         bestscore[e] = np.argmax(sc)
                         bestdist[e] = dist
-                    if xe < o_bestl2[e] and compare_single(sc, lab) and (dist > 4000 and dist < 10000):
+                    if xe < o_bestl2[e] and compare_single(sc, lab) and (dist > 5000 and dist < 10000):
                         o_bestl2[e] = xe
                         o_bestscore[e] = np.argmax(sc)
                         o_bestattack[e] = ii
