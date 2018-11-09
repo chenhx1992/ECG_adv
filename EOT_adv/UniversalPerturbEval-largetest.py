12eb11905ef88fcfc2e6112f41c257109f007e82
hcyray <chuangak@connect.ust.hk>
2018-11-10 01:48:10 +0800

    Update UniversalPerturbEval-largetest.py

--- EOT_adv/UniversalPerturbEval-largetest.py
+++ EOT_adv/UniversalPerturbEval-largetest.py
@@ -34,7 +34,7 @@ def op_concate(x, w, p):
     return np.append(x2, x1, axis=1)
 
 def op_concate2(x, w, p):
-    data_len = 4000
+    data_len = 4500
     tile_times = math.ceil(data_len/w)
     x_tile = np.tile(x, (1, tile_times, 1))
     x1 = np.zeros((1,9000,1))
