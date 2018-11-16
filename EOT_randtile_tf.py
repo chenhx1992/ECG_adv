ff80fc290fca94f69e1c51833cc2606c876587e2
hcyray <chuangak@connect.ust.hk>
2018-11-16 22:35:42 +0800

    update

--- EOT_randtile_tf.py
+++ EOT_randtile_tf.py
@@ -345,7 +345,7 @@ class EOT_tf_ATTACK(object):
                 # adjust the best result found so far
                 for e, (l2, sc, ii, dist, xe) in enumerate(zip(itertools.repeat(l, len(scores)), scores, nimg, l2s, xent)):
                     lab = np.argmax(batchlab[e])
-                    if xe < bestl2[e] and compare(sc, lab):
+                    if xe < bestl2[e] and compare(sc, lab) and (dist > 4500 and dist < 10000):
                         bestl2[e] = xe
                         bestscore[e] = np.argmax(sc)
                         bestdist[e] = dist
