b56daf87f4268cfafc0deebd0b3e3a369d4aa28b
hcyray <chuangak@connect.ust.hk>
2019-02-25 15:29:12 +0800

    update

--- temp.py
+++ temp.py
@@ -1,22 +1,9 @@
+import csv
+import glob
 
-
-
-import tensorflow as tf
-
-print(tf.__version__)
-sess = tf.InteractiveSession()
-tf_dtype = tf.as_dtype('int32')
-
-# Some tensor we want to print the value of
-
-a = tf.constant(3)
-b = tf.constant([1,2,3])
-c = tf.shape(b)[0]
-
-d = tf.cond(tf.equal(a,c),lambda: tf.constant(0), lambda: a)
-
-
-
-print(c.eval())
-print(a.eval())
-print(d.eval())
+dataDir = './training_raw/'
+# load groundTruth
+print("Loading ground truth file")
+csvfile = list(csv.reader(open('REFERENCE-v3.csv')))
+files = sorted(glob.glob(dataDir + "*.mat"))
+print(files)
