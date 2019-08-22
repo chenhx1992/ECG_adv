5b5e699b20f67c1176e1e277dd2af3c0a5c9900a
hcy_ray <hcy202415>
2019-08-22 21:42:27 +0800

    update

--- temp.py
+++
@@ -1,9 +0,0 @@
-import csv
-import glob
-
-dataDir = './training_raw/'
-# load groundTruth
-print("Loading ground truth file")
-csvfile = list(csv.reader(open('REFERENCE-v3.csv')))
-files = sorted(glob.glob(dataDir + "*.mat"))
-print(files)
